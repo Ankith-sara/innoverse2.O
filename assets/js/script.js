f7e90f41ece86e742758435f8a37b5b6fa1ba4b8
Ankith Kumar Sara <ankithkumar.sara_2026@woxsen.edu.in>
2025-02-06 10:58:38 +0530

    Update message

--- assets/js/script.js
+++ assets/js/script.js
@@ -224,3 +224,23 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+// Get the button
+let scrollTopBtn = document.getElementById("scroll-top-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollTopBtn.classList.add("show");
+    } else {
+        scrollTopBtn.classList.remove("show");
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+scrollTopBtn.onclick = function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
